429747dd0bc137218a968018b8883483ecc3cd42
jaydeepjay <jaydipjejariya07@gmail.com>
2026-02-26 20:12:06 +0530

    Add files via upload

---
+++ error.py
@@ -0,0 +1,16 @@
+print("Sports Registration")
+sports = ["Cricket","Football","Tennis"]
+players = []
+name = input("Enter name: ")
+sport = input("Enter sport: ")
+if sport in sport:
+ players.append(name)
+else:
+ print("Sport not available")
+
+print("Player:", name)
+print("Sport:", Sport)
+
+for i in range(3):
+ print("Registered")
+print("End")
